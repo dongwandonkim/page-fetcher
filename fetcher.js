f66483e601430b95070a10af5ee8d5ca5b6c4822
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-05-11 22:35:12 -0700

    all stretches

--- fetcher.js
+++ fetcher.js
@@ -1,14 +1,24 @@
 const fs = require('fs');
 const request = require('request');
 const readline = require('readline');
+
 const rl = readline.createInterface(process.stdin, process.stdout);
 
 const args = process.argv.slice(2);
-const paths = args[1].slice(0, 2);
-
-let answer;
+const paths = args[1].split('/');
 
 const getFileFromServer = (args) => {
+  // check if path is valid
+  if (paths.length > 2) {
+    fs.access(`./${paths[1]}`, function (error) {
+      if (error) {
+        console.log('invalid path:', error);
+        process.exit();
+      }
+      console.log('valid path');
+    });
+  }
+
   request.get(args[0], (err, res, body) => {
     if (err) {
       console.log(err);
@@ -18,35 +28,30 @@ const getFileFromServer = (args) => {
     if (fs.existsSync(args[1])) {
       rl.question('file exists overwrite? ', (input) => {
         if (input.toLowerCase() === 'y') {
-          answer = true;
+          writeFileToLocal(body);
           rl.close();
         } else if (input.toLowerCase() === 'n') {
-          answer = false;
-          rl.close();
           process.exit();
         }
       });
+    } else {
+      writeFileToLocal(body);
+      rl.close();
     }
-
-    if (answer) {
-      fs.writeFile(args[1], body, (err) => {
-        if (err) return console.log(err);
-
-        fs.access(paths, function (error) {
-          if (error) {
-            return console.log(error);
-          }
-          console.log('valid path');
-        });
-
-        fs.stat(args[1], (err, stat) => {
-          if (err) return console.log(err);
-
-          console.log(`Downloaded and saved ${stat.size} bytes to ${args[1]}`);
-        });
-      });
-    }
+    return;
   });
 };
 
 getFileFromServer(args);
+
+const writeFileToLocal = (body) => {
+  fs.writeFile(args[1], body, (err) => {
+    if (err) return console.log('invalid to write:', err);
+
+    fs.stat(args[1], (err, stat) => {
+      if (err) return console.log(err);
+
+      console.log(`Downloaded and saved ${stat.size} bytes to ${args[1]}`);
+    });
+  });
+};
